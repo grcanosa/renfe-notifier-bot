dcdbcf74b4a8b052e7147f3bbc0cd532660d4c1e
grcanosa <grcanosa@gmail.com>
2018-07-16 20:23:31 +0200

    Modified texts

--- python/texts.py
+++ python/texts.py
@@ -20,9 +20,9 @@ texts_raw = {
 "MAIN_OP_UNKNOWN": "Lo siento, no te he entendido. ",
 "DO_ONETIME_QUERY": "Ok, voy a hacer una consulta puntual.",
 "ADD_PERIODIC_QUERY": "Ok, voy a añadir una consulta periódica.",
-"SELECT_TRIP_TO_DETELE": "Por favor, selecciona una consulta para eliminar",
-"SELECT_ORIGIN_STATION": "Introduce estación de origen.",
-"SELECT_DESTINATION_STATION": "Introduce estación de destino.",
+"SELECT_TRIP_TO_DETELE": "Por favor, selecciona una consulta para eliminar.",
+"SELECT_ORIGIN_STATION": "Introduce estación de origen :station:.",
+"SELECT_DESTINATION_STATION": "Introduce estación de destino :station:.",
 "SELECT_TRIP_DATE": "Elige ahora la fecha del viaje.",
 "SELECTED_TRIP": "Has seleccionado el trayecto {origin} -> {destination}",
 "SELECTED_DATA": "Perfecto, has seleccionado el trayecto {origin} -> {destination}"
